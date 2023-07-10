9fcb11e5f38b777f7c4889c55f1627d595874ec9
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2023-07-11 01:33:12 +0900

    ⚡️ templateにfunctionを追加

--- golang/main.go
+++ golang/main.go
@@ -2,3 +2,30 @@ package main
 
 func main() {
 }
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func min(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret > v {
+			ret = v
+		}
+	}
+	return ret
+}
+
+func max(values ...int) int {
+	ret := values[0]
+	for _, v := range values {
+		if ret < v {
+			ret = v
+		}
+	}
+	return ret
+}
