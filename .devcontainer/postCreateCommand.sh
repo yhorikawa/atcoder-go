1b41c0a6cfb5be89ed3143114b562b77949c99b8
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-01-07 22:22:42 +0900

    🔧 npm installとpip installをdevcontainer featuresに任せた

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-
-pip install online-judge-tools
-
-npm install -g atcoder-cli
-
 mkdir -p /home/vscode/.config/atcoder-cli-nodejs
 cp -r golang /home/vscode/.config/atcoder-cli-nodejs/golang
 acc config default-test-dirname-format test
