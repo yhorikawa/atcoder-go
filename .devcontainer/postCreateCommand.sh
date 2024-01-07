4f65b8d8ee9673dd020d5f5c324cfacf3e1b172d
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-01-07 22:31:12 +0900

    🔧 postcreateComand.shを使わずにSetupするように変更

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p /home/vscode/.config/atcoder-cli-nodejs
-cp -r golang /home/vscode/.config/atcoder-cli-nodejs/golang
-acc config default-test-dirname-format test
-acc config default-task-choice all
-acc config default-template golang
